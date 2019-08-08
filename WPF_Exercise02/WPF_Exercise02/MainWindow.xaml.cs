a0c836e6c1998fd11dbf25800824bca8300707cf
Rossal1 <Rossal1@g.toiohomai.ac.nz>
2019-08-08 13:25:13 +1200

    Exercise3_01

---
+++ WPF_Exercise02/WPF_Exercise02/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Exercise02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, string[]> cat1 = new Dictionary<string, string[]>();
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            string[] cats = new string[3]("Apple", "Banana", "Orange");
+            cat1.ItemsSource = cats;
+            cat1.Add("1", new string[] { "1", "2" });
+            img1.Source = new BitmapImage(new Uri("C:/Users/rossy/Desktop/WPF/WPF_Exercise02/WPF_Exercise02/images/cats01.jpg", UriKind.RelativeOrAbsolute));
+            img2.Source = new BitmapImage(new Uri("C:/Users/rossy/Desktop/WPF/WPF_Exercise02/WPF_Exercise02/images/cats02.jpg", UriKind.RelativeOrAbsolute));
+            item1.Content = "Selected";
+        }
+    }
+    public class MyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return string.Format("item.{0}.png", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Wpf_Exercise03_01/Wpf_Exercise03_01/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Exercise03_01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SliderChanged();
+        }
+
+        private void RChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SolidColorBrush myBrush = (SolidColorBrush)this.Resources["myBrush"];
+
+            myBrush.Color = Color.FromRgb((byte)RSlider.Value, (byte)GSlider.Value, (byte)BSlider.Value);
+
+            byte r = (byte)RSlider.Value;
+            byte g = (byte)GSlider.Value;
+            byte b = (byte)BSlider.Value;
+
+            hex.Text = string.Format("#{0:X2}{1:X2}{2:X2}", r, g, b);
+        }
+
+        private void GChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SolidColorBrush myBrush = (SolidColorBrush)this.Resources["myBrush"];
+
+            myBrush.Color = Color.FromRgb((byte)RSlider.Value, (byte)GSlider.Value, (byte)BSlider.Value);
+
+            byte r = (byte)RSlider.Value;
+            byte g = (byte)GSlider.Value;
+            byte b = (byte)BSlider.Value;
+
+            hex.Text = string.Format("#{0:X2}{1:X2}{2:X2}", r, g, b);
+        }
+
+        private void BChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SolidColorBrush myBrush = (SolidColorBrush)this.Resources["myBrush"];
+
+            myBrush.Color = Color.FromRgb((byte)RSlider.Value, (byte)GSlider.Value, (byte)BSlider.Value);
+
+            byte r = (byte)RSlider.Value;
+            byte g = (byte)GSlider.Value;
+            byte b = (byte)BSlider.Value;
+
+            hex.Text = string.Format("#{0:X2}{1:X2}{2:X2}", r, g, b);
+        }
+        public void SliderChanged()
+        {
+            byte r = (byte)RSlider.Value;
+            byte g = (byte)GSlider.Value;
+            byte b = (byte)BSlider.Value;
+
+            hex.Text = string.Format("#{0:X2}{1:X2}{2:X2}", r, g, b);
+
+        }
+    }
+}
